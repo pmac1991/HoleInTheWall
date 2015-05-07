8ecec39ef6ae27326e23bceda7ece8649b86ef7b
pmac1991 <piomac11@gmail.com>
2015-05-07 17:52:33 +0200

    Initialization pated from OpneGL test sample, works for two skeletons, still frame issues with one skeleton !!!

--- HoleInTheWall/HoleInTheWall.cpp
+++ HoleInTheWall/HoleInTheWall.cpp
@@ -1,4 +1,4 @@
-// HoleInTheWall.cpp : Defines the entry point for the console application.
+﻿// HoleInTheWall.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -13,6 +13,8 @@
 #include <NuiImageCamera.h>
 #include <NuiSensor.h>
 
+#include <iostream>
+
 #define width 640
 #define height 480
 
@@ -20,13 +22,13 @@
 
 GLuint textureId;              // ID of the texture to contain Kinect RGB Data
 
-GLubyte data[width*height * 4];  // BGRA array containing the texture data
-
 // Kinect variables
 
 HANDLE rgbStream;              // The identifier of the Kinect's RGB Camera
 
 INuiSensor* sensor;            // The kinect sensor
+
+int iterator = 0;
 
 struct jointScreenCo{
 	float positionX;
@@ -42,7 +44,7 @@ bool initKinect() {
 	if (NuiCreateSensorByIndex(0, &sensor) < 0) return false;
 
 	// Initialize sensor
-	sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH | NUI_INITIALIZE_FLAG_USES_COLOR | NUI_INITIALIZE_FLAG_USES_SKELETON);
+	sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH | NUI_INITIALIZE_FLAG_USES_SKELETON);
 /*	sensor->NuiImageStreamOpen(
 		NUI_IMAGE_TYPE_COLOR,            // Depth camera or rgb camera?
 		NUI_IMAGE_RESOLUTION_640x480,    // Image resolution
@@ -60,6 +62,8 @@ void DrawBone(const NUI_SKELETON_DATA & skel, NUI_SKELETON_POSITION_INDEX joint0
 {
 	NUI_SKELETON_POSITION_TRACKING_STATE joint0State = skel.eSkeletonPositionTrackingState[joint0];
 	NUI_SKELETON_POSITION_TRACKING_STATE joint1State = skel.eSkeletonPositionTrackingState[joint1];
+
+	std::cout << "Jestem w drawBonr!!";
 
 	// If we can't find either of these joints, exit
 	if (joint0State == NUI_SKELETON_POSITION_NOT_TRACKED || joint1State == NUI_SKELETON_POSITION_NOT_TRACKED)
@@ -80,21 +84,25 @@ void DrawBone(const NUI_SKELETON_DATA & skel, NUI_SKELETON_POSITION_INDEX joint0
 		jointScreenCo temp1 = m_Points[joint1];
 
 		//m_pRenderTarget->DrawLine(m_Points[joint0], m_Points[joint1], m_pBrushBoneTracked, g_TrackedBoneThickness);
-		glBegin(GL_LINES);
-			glVertex3f(temp0.positionX, temp0.positionY, 0);
-			glVertex3f(temp1.positionX, temp1.positionY, 0);
-		glEnd();
+		glPushMatrix();
+			glBegin(GL_LINES);
+				glVertex3f(temp0.positionX, temp0.positionY, 0);
+				glVertex3f(temp1.positionX, temp1.positionY, 0);
+			glEnd();
+		glPopMatrix();
+		
 	}
 	else
 	{
 		//m_pRenderTarget->DrawLine(m_Points[joint0], m_Points[joint1], m_pBrushBoneInferred, g_InferredBoneThickness);
 		jointScreenCo temp0 = m_Points[joint0];
 		jointScreenCo temp1 = m_Points[joint1];
-
-		glBegin(GL_LINES);
-			glVertex3f(temp0.positionX, temp0.positionY, 0);
-			glVertex3f(temp1.positionX, temp1.positionY, 0);
-		glEnd();
+		glPushMatrix();
+			glBegin(GL_LINES);
+				glVertex3f(temp0.positionX, temp0.positionY, 0);
+				glVertex3f(temp1.positionX, temp1.positionY, 0);
+			glEnd();
+		glPopMatrix();
 	}
 }
 
@@ -177,7 +185,7 @@ void ProcessSkeleton(){
 		if (NUI_SKELETON_TRACKED == trackingState)
 		{
 			// We're tracking the skeleton, draw it
-			DrawSkeleton(skeletonFrame.SkeletonData[i], width, height);
+		    DrawSkeleton(skeletonFrame.SkeletonData[i], width, height);
 		}
 		else if (NUI_SKELETON_POSITION_ONLY == trackingState)
 		{
@@ -195,104 +203,64 @@ void ProcessSkeleton(){
 }
 
 
-void getKinectData(GLubyte* dest) {
-	NUI_IMAGE_FRAME imageFrame;
-	NUI_LOCKED_RECT LockedRect;
-	if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0) return;
-	INuiFrameTexture* texture = imageFrame.pFrameTexture;
-	texture->LockRect(0, &LockedRect, NULL, 0);
-
-	if (LockedRect.Pitch != 0)
-	{
-		const BYTE* curr = (const BYTE*)LockedRect.pBits;
-		const BYTE* dataEnd = curr + (width*height) * 4;
-
-		while (curr < dataEnd) {
-			*dest++ = *curr++;
-		}
-	}
-
-	texture->UnlockRect(0);
-	sensor->NuiImageStreamReleaseFrame(rgbStream, &imageFrame);
-}
-
-void drawKinectData() {
-	glBindTexture(GL_TEXTURE_2D, textureId);
-	getKinectData(data);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)data);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_QUADS);
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f(0, 0, 0);
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f(width, 0, 0);
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f(width, height, 0.0f);
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f(0, height, 0.0f);
-	glEnd();
-}
 
 void draw() {
-	//drawKinectData();
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, width, height, 0, 1, -1);
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
 	// czyszczenie bufora koloru
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// wyb�r macierzy modelowania
+	// wybór macierzy modelowania
 	glMatrixMode(GL_MODELVIEW);
 
 	// macierz modelowania = macierz jednostkowa
 	glLoadIdentity();
 
 	ProcessSkeleton();
-	//glFlush();
+
+	glPushMatrix();
+	glBegin(GL_TRIANGLES);
+		glVertex3f(0,0,0);
+		glVertex3f(0, 1, 0);
+		glVertex3f(1,0,0);
+	glEnd();
+	glPopMatrix();
+
+	// kolor krawêdzi szeœcianu
+
+	// skierowanie poleceñ do wykonania
+	glFlush();
+
+	// zamiana buforów koloru
 	glutSwapBuffers();
-
-}
-
-void execute() {
-	glutMainLoop();
-}
-
-bool init(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(width, height);
-	glutCreateWindow("Kinect SDK Tutorial");
-	glutDisplayFunc(draw);
-	glutIdleFunc(draw);
-	return true;
 }
 
 int main(int argc, char* argv[])
 {
-	if (!init(argc, argv)) return 1;
 	if (!initKinect()) return 1;
 
-	// Initialize textures
-	glGenTextures(1, &textureId);
-	glBindTexture(GL_TEXTURE_2D, textureId);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height,
-		0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)data);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	// inicjalizacja biblioteki GLUT
+	glutInit(&argc, argv);
 
-	// OpenGL setup
-	glClearColor(0, 0, 0, 0);
-	glClearDepth(1.0f);
-	glEnable(GL_TEXTURE_2D);
+	// inicjalizacja bufora ramki
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
-	// Camera setup
-	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, width, height, 0, 1, -1);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	// rozmiary g³ównego okna programu
+	glutInitWindowSize(width, height);
+
+	glutCreateWindow("Kinect");
+
+	glutDisplayFunc(draw);
+
+	glutIdleFunc(draw);
 
 	// Main loop
-	execute();
+	glutMainLoop();
 	return 0;
 }
 
